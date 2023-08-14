f0876474cab7383069129c0581fa1ed75c4b1e6e
kdshola <khidirshola@gmail.com>
2023-08-14 09:33:53 +0100

    add c and python program and function files

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <Python.h>
+#include "Python.h"
 
 /**
  * print_python_list_info - prints python list information
@@ -10,14 +9,14 @@
 
 void print_python_list_info(PyObject *p)
 {
-	int size = Py_SIZE(p), i = 0;
+	int i = 0;
 	PyListObject *list;
 	PyObject *item;
 
-	printf("[*] Size of the Python List = %d\n", size);
 	list = (PyListObject *)p;
-	printf("[*] Allocated = %d\n", list->allocated);
-	for (; i < size; i++)
+	printf("[*] Size of the Python List = %ld\n", list->ob_base.ob_size);
+	printf("[*] Allocated = %ld\n", list->allocated);
+	for (; i < list->ob_base.ob_size; i++)
 	{
 		item = PyList_GetItem(p, i);
 		printf("Element %d: %s\n", i, Py_TYPE(item)->tp_name);
