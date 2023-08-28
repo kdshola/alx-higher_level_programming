3dee76d164e19f317445e6977739b67a9d4f0377
kdshola <khidirshola@gmail.com>
2023-08-28 16:07:18 +0100

    add c and python function files

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import sys
 def safe_function(fct, *args):
+    import sys
     try:
         res = fct(*args)
-        return res
     except Exception as error:
         print("Exception: {}".format(error), file=sys.stderr)
-        return None
+        res = None
+    return res
