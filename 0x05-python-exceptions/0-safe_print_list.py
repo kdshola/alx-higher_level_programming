c95b2fa441280f7df48e765d5ac8b5d39e237983
kdshola <khidirshola@gmail.com>
2023-08-28 14:08:31 +0100

    add 0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    total = 0
+    for num in range(x):
+        try:
+            print(my_list[num], end='')
+            total += 1
+        except IndexError:
+            break
+        print()
+        return total
