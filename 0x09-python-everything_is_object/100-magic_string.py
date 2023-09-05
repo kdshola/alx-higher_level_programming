22d90c4b04d986fa4938d6b93f9fa24287f02456
kdshola <khidirshola@gmail.com>
2023-09-05 17:24:43 +0100

    add task answers

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,1 +1,4 @@
 #!/usr/bin/python3
+def magic_string(H=[]):
+    magic_string.count = getattr(magic_string, "count", 0) + 1
+    return ("BestSchool, " * (magic_string.count - 1) + "BestSchool")
--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""
+    A restricted class module
+"""
+
+
+class LockedClass:
+    """ A class that limits dynamic creation of its instance """
+    __slots__ = "first_name"
