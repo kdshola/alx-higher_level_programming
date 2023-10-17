c10c00c39fdcf9644d51ada9cdb42c492294a762
kdshola <khidirshola@gmail.com>
2023-10-17 18:22:57 +0100

    ADD mysql queries files

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- Inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL
+-- 	server values id = 89, name = Best School.
+
+INSERT INTO `first_table` VALUES (89, 'Best School');
