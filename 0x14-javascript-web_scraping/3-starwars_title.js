7459c93dedc809ef80ef1c526051169ba530b197
kdshola <khidirshola@gmail.com>
2024-01-03 21:05:05 +0100

    add request query scripts

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const request = require('request');
+const episodeNum = process.argv[2];
+const API_URL = 'https://swapi-api.hbtn.io/api/films/';
+
+request(API_URL + episodeNum, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const respJSON = JSON.parse(body);
+    console.log(respJSON.title);
+  } else {
+    console.log('Error code: ' + response.statusCode);
+  }
+});
